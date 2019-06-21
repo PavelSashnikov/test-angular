e37b9ff5b350fd42d3ff542ca774bc28baa0a65f
PavelSashnikov <Pasha_sss@tut.by>
2019-06-21 12:27:24 +0300

    styles

--- src/app/list/list.component.ts
+++ src/app/list/list.component.ts
@@ -1,9 +1,13 @@
 import { Component, OnInit } from '@angular/core';
+interface Tag {
+  text: string;
+  selected:boolean;
+}
 
-// interface INotes {
-//   title: string;
-  
-// }
+interface Note {
+  title: string;
+  tags: Tag[];
+}
 
 @Component({
   selector: 'app-list',
@@ -12,40 +16,60 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListComponent implements OnInit {
 
-  notes = [
+  notes: Note[] = [
     {
       title: 'it`s #title',
-      tags: ['#title']
+      tags: [
+        {
+          text: '#title',
+          selected: false,
+        }
+      ]
     }
   ];
+  hashtag: RegExp = /\#\w\w+\b/g;
 
   addNote(noteText) {
     var newNote = {
       title: noteText,
-      tags: noteText.match(/\#\w\w+\b/g)
+      tags: []
     };
-    noteText = '';
+    if(noteText.match(this.hashtag) !== null) {
+      newNote.tags = noteText.match(this.hashtag).map(tag => ({text:tag,selected:false}))
+    }
     this.notes.push(newNote);
   }
+
   deleteNote(note) {
     this.notes = this.notes.filter( el => el.title !== note.title)
   }
+
   changeNote(note) {
     note.title = prompt('new title');
-    note.tags = note.title.match(/\#\w\w+\b/g)
+    note.tags = note.title.match(this.hashtag)
   }
+
   showTags(tag) {
-    this.notes = this.notes.filter( el => el.tags.includes(tag));
+    this.notes = this.notes.filter( el => el.tags.some(currentTag =>
+      currentTag.text === tag));
   }
-  deleteTags(note) {
-    note.tags = [];
+
+  deleteTags(noteID) {
+    this.notes[noteID].tags = [];
   }
-  addTag(note) {
-   var newTag = prompt('add tag');
-    note.tags += note.tags.push(newTag);
+
+  addTag(noteID) {
+   const newTag = prompt('add tag');
+   this.notes[noteID].tags.push({text:newTag,selected:false});
   }
-  serchTag(newNote) {
-    console.log(newNote);
+
+  searchTag(newNote) {
+    const selectedTag:string[] = newNote.match(this.hashtag);
+    if(!selectedTag) return;
+    this.notes.forEach(note => {
+      note.tags.forEach(tag =>
+        tag.selected = selectedTag.includes(tag.text))
+    })
   }
 
   constructor() { }
